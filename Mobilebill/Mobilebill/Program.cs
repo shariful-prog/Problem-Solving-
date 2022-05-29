2ae3f0adb0f946de2efbc60fc4481cb603ede51d
Shariful <shariful.gakk@gmail.com>
2022-05-29 10:52:50 +0600

    Intialized

---
+++ Mobilebill/Mobilebill/Program.cs
@@ -0,0 +1,40 @@
+﻿
+// Not completed -  Running
+
+// Calculating peak and off peak hour between two dates
+// 30 second pulse 
+// Author : Shariful Islam
+
+
+
+DateTime startDate = DateTime.Parse("2021-01-30 08:00:13 am"); 
+DateTime endDate = DateTime.Parse("2021-01-31 10:00:39 am");
+
+
+var peakHourStart = Convert.ToDateTime("9.00.00 am").TimeOfDay;
+var peakHourEnd = Convert.ToDateTime("10.59.59 pm").TimeOfDay;
+
+var offPeakHourStart = Convert.ToDateTime("11.00.00 pm").TimeOfDay;
+var offPeakHourEnd = Convert.ToDateTime("8.59.59 am").TimeOfDay;
+
+int count = 0;
+while(startDate <= endDate)
+{
+    if(count !=0) startDate = startDate.AddSeconds(1);
+    var tempTime = startDate.AddSeconds(30).TimeOfDay;
+    if(tempTime >= peakHourStart && tempTime <= peakHourEnd)
+    {
+        Console.WriteLine(tempTime.ToString()+"("+ peakHourStart.ToString() +"-"+peakHourEnd.ToString()+ ")"+" = Peak hour");
+    }
+    else
+    {
+        Console.WriteLine(tempTime.ToString() + "(" + offPeakHourStart.ToString() + "-" + offPeakHourEnd.ToString() + ")" + " = OffPeak hour");
+
+    }
+
+    startDate = startDate.AddSeconds(30);
+    count++;
+}
+
+
+
